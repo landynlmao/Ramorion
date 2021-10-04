9c7af5980f4739c79289f4175e31a2084726c960
Landynlol <Landynlol@users.noreply.github.com>
2021-10-04 04:41:51 -0500

    First Commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import DisordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DisordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES    
+    ]
+})
+
+client.on('ready', () => {
+    console.log('the bot is ready!')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
